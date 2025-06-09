6d4711ac0079d4d99aaf33dc9b21af34adc992d4
victor <vrubinr501@gmail.com>
2025-06-09 21:12:13 +0200

    09/06/2025

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.aplicacion.aplicacionfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.aplicacion.aplicacionfx to javafx.fxml;
-    exports com.aplicacion.aplicacionfx;
-}

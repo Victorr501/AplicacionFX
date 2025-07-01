2c32cd4ed88304b2c97ee47a9cff6a22ea0b6e60
victor <vrubinr501@gmail.com>
2025-07-01 15:11:39 +0200

    01/07/2025 15:11

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module AplicacionFX {
     // Abre el paquete de tus controladores para que FXMLLoader pueda instanciarlos y inyectar @FXML
     opens com.aplicacion.aplicacionfx.vista.usuarios to javafx.fxml;
     opens com.aplicacion.aplicacionfx.vista.registro to javafx.fxml;
+    opens com.aplicacion.aplicacionfx.vista.actualizar to javafx.fxml;
     opens com.aplicacion.aplicacionfx.cliente.dto to com.google.gson;
 
     // Abre el paquete de tus clases de modelo (UsuarioUI) para que JavaFX.base pueda acceder vía reflexión
